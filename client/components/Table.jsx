ba447ddd9905a0007b386d7915d0bc5dd285f206
Derek Lauziere <dereklauziere@Dereks-Air.home>
2020-05-09 11:01:42 -0400

    fixed names

--- client/components/Table.jsx
+++ client/components/Table.jsx
@@ -1,6 +1,8 @@
+import Field from "/Field.jsx";
+
 export default function FieldContainer() {
-  const elements = this.props.fields.map((el, i) => {
-    return <TableStuff key={i} info={el} />;
+  const fields = this.props.fields.map((el, i) => {
+    return <Field key={i} info={el} props={this.props} />;
   });
 
   return (
@@ -9,14 +11,14 @@ export default function FieldContainer() {
       <button className="changeName" onClick={props.update(name)}>
         Change Name
       </button>
-      <button className="moveBox" onClick={props.move}>
+      <button className="moveBox" onClick={props.move()}>
         Move
       </button>
-      <button className="addField" onClick={props.add}>
+      <button className="addField" onClick={props.add()}>
         Add Field
       </button>
       <div className="fieldsGoHere">{fields}</div>
-      <button className="RemoveField" onClick={props.remove}>
+      <button className="RemoveField" onClick={props.remove(i)}>
         Remove Field
       </button>
     </div>
