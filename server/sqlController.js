7f244c03ba81bf9bfa45f0bcf99c4286f622a23e
Henry Black <blackhaj@gmail.com>
2020-05-10 15:51:04 -0500

    Tweaks to SQL export request

--- server/sqlController.js
+++ server/sqlController.js
@@ -1,22 +1,21 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const sqlController = (req, res, next) => {
   if (!req.body) {
-    res.status(422).json({message: "No data to process"});
+    res.status(422).json({ message: "No data to process" });
   }
-  fs.writeFileSync('./server/files/tables.txt', JSON.stringify(req.body), 'utf8');
+  fs.writeFileSync(
+    "./server/files/tables.txt",
+    JSON.stringify(req.body),
+    "utf8"
+  );
 
   console.log("in request");
-  // res.status(200).sendFile(path.resolve(__dirname, './files/tables.txt'), {
-  //   headers: {
-  //     "content": "text/html;charset=utf-8",
-  //     "Content-Disposition": "attachment; filename=tables.txt",
-  //     "Content-Type": 'application/force-download',
-  //   },
-  // });
-  res.download(path.resolve(__dirname, './files/tables.pdf'), "tables.pdf");
+  res.set("Content-Disposition", "inline;filename=tables.txt");
+  res.set("Content-Type", "text/plain")
+  res.download(path.join(__dirname, './files/tables.txt'));
+  
 };
-
 
 module.exports = sqlController;
