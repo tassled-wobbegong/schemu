3ebbd5aba43e0032524441cc852226cf739893b1
Madison Brown <mbrown3391@gmail.com>
2020-07-24 18:37:58 -0400

    removed deployment files

--- server/secret.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  MONGO_URI: 'mongodb://schemu:8lTGM4So4bhIjjmI@cluster0-shard-00-00-n177k.mongodb.net:27017,cluster0-shard-00-01-n177k.mongodb.net:27017,cluster0-shard-00-02-n177k.mongodb.net:27017/Cluster0?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
-};
